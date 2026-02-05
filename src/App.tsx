5fa859914e3766c48ccd4dde30d00916363d7a72
Benbow <benrocks2005@gmail.com>
2026-02-04 22:13:14 -0500

    task 3 finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                Hello World UD CISC275 with React Hooks and TypeScript By: Benjamin Bowman
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <Container>
+            <Row>
+                <Col>
+                    <header className="App-header">
+                        <h1>Hello World UD CISC275 with React Hooks and TypeScript By: Benjamin Bowman</h1>
+                    </header>
+                    <img src="" alt="this is probably a image" />
+                    <div style={{color: 'red', width: '10px', height: '5px'}}></div>
+                </Col>
+                <Col>
+                <Button onClick={() => console.log("Hello World!")}>Log Hello World</Button>
+                    <ul>
+                        <li>First thing</li>
+                        <li>Another thing</li>
+                        <li>A third item</li>
+                    </ul>
+                    <div style={{backgroundColor: 'red', width: '10px', height: '5px'}}></div>
+                </Col>
+            </Row>
+        </Container>
         </div>
     );
 }
